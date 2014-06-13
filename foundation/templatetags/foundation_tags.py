e2b91cdcb401027821151366e9b83344fc33784e
Yar Kravtsov <yarlson@gmail.com>
2014-06-13 11:10:03 +0400

    renames template to jinja extension

--- foundation/templatetags/foundation_tags.py
+++ foundation/templatetags/foundation_tags.py
@@ -1,22 +1,25 @@
-from django import template
 from django.templatetags.static import static
+from django_jinja import library
 
-register = template.Library()
+lib = library.Library()
 
-@register.simple_tag
+
+@lib.global_function
 def foundation_js(js_name=None):
     js_path = "foundation/js/foundation/foundation.{0}.js".format(js_name)
     if js_name is None:
-       js_path = "foundation/js/foundation.min.js".format(js_name)
+        js_path = "foundation/js/foundation.min.js".format(js_name)
 
     return '<script src="{0}"></script>'.format(static(js_path))
 
-@register.simple_tag
+
+@lib.global_function
 def foundation_vendor(vendor_name):
     vendor_path = "foundation/js/vendor/{0}.js".format(vendor_name)
     return '<script src="{0}"></script>'.format(static(vendor_path))
 
-@register.simple_tag
+
+@lib.global_function
 def foundation_css(css_name):
     css_path = "foundation/css/{0}.css".format(css_name)
     return '<link rel="stylesheet" href="{0}"/>'.format(static(css_path))
